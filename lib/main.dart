8944ff70808b915ca0916d7697604585f611a9e8
hyunseok <dopocoo@naver.com>
2023-04-03 15:54:02 +0900

    StateNotifierProvider

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class Counter extends StateNotifier<int>{
+  Counter(int num) : super(num);
+
+  void increment(){
+    state++;
+  }
+}
+
+final counterProvider = StateNotifierProvider<Counter,int>((ref) {
+return Counter(0);
+});
+
+void main() {
+  runApp(
+    // For widgets to be able to read providers, we need to wrap the entire
+    // application in a "ProviderScope" widget.
+    // This is where the state of our providers will be stored.
+    ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+// Extend ConsumerWidget instead of StatelessWidget, which is exposed by Riverpod
+class MyApp extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final int value = ref.read(counterProvider);
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Example')),
+        body: Center(
+          child: Text("$value"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            ref.watch(counterProvider.notifier).increment();
+          },
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
